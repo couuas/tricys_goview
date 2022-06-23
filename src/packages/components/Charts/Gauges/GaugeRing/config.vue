7cf6eda3b17b6de4dcee422e4ca138762b1390dc
wangzhiqiang <wangzhiqiang@qq.com>
2022-06-23 16:44:56 +0800

    配置项

--- src/packages/components/Charts/Gauges/GaugeRing/config.vue
+++ src/packages/components/Charts/Gauges/GaugeRing/config.vue
@@ -1,48 +1,16 @@
 <template>
   <!-- 遍历 seriesList -->
   <CollapseItem :name="`圆环`" :expanded="true">
-    <!-- <SettingItemBox name="数据样式">
-      <SettingItem :name="item.name+'字体大小'" v-for="(item,index) in config.title" :key="index">
-        <n-input-number v-model:value="item.textStyle.fontSize" :min="10" :step="1" size="small" placeholder="数值">
+    <SettingItemBox name="样式">
+      <SettingItem name="环宽度">
+        <n-input-number v-model:value="config.series[0].axisLine.lineStyle.width" :min="10" :step="1" size="small" placeholder="数值">
         </n-input-number>
       </SettingItem>
-      <SettingItem :name="item.name+'字体宽度'" v-for="(item,index) in config.title" :key="index">
-        <n-input-number v-model:value="item.textStyle.fontWeight" :min="100" :step="100" size="small" placeholder="数值">
+      <SettingItem name="字体大小">
+        <n-input-number v-model:value="config.series[0].detail.fontSize" :min="10" :step="1" size="small" placeholder="数值">
         </n-input-number>
       </SettingItem>
     </SettingItemBox>
-
-    <SettingItemBox name="进度条">
-      <SettingItem name="前景色">
-        <n-color-picker
-          size="small"
-          :modes="['hex']"
-          v-model:value="config.series[0].itemStyle.color"
-        ></n-color-picker>
-      </SettingItem>
-      <SettingItem name="背景色">
-        <n-color-picker
-          size="small"
-          :modes="['hex']"
-          v-model:value="config.series[0].backgroundStyle.color"
-        ></n-color-picker>
-      </SettingItem>
-      <SettingItem name="外边框">
-        <n-color-picker
-          size="small"
-          :modes="['hex']"
-          v-model:value="config.series[1].itemStyle.borderColor"
-        ></n-color-picker>
-      </SettingItem>
-      <SettingItem name="内边框">
-        <n-color-picker
-          size="small"
-          :modes="['hex']"
-          v-model:value="config.series[2].itemStyle.borderColor"
-        ></n-color-picker>
-      </SettingItem>
-    </SettingItemBox> -->
-    
   </CollapseItem>
 </template>
 
