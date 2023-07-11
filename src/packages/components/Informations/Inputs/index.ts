5c69a83e90e9eb37fd5630766a794a54a4bfd420
luoyp <2456821174@qq.com>
2023-07-11 09:20:18 +0800

    新增input联动控制组件

--- src/packages/components/Informations/Inputs/index.ts
+++ src/packages/components/Informations/Inputs/index.ts
@@ -1,5 +1,6 @@
 import { InputsDateConfig } from './InputsDate/index'
 import { InputsSelectConfig } from './InputsSelect/index'
 import { InputsTabConfig } from './InputsTab/index'
+import {InputsInputConfig} from './InputsInput/index'
 
-export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig]
+export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig,InputsInputConfig]
