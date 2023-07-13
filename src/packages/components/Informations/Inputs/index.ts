066294ff7e8ea65f5f63a093410c15bd192f69a0
luoyp <2456821174@qq.com>
2023-07-13 16:47:19 +0800

    新增分页组件

--- src/packages/components/Informations/Inputs/index.ts
+++ src/packages/components/Informations/Inputs/index.ts
@@ -2,5 +2,6 @@ import { InputsDateConfig } from './InputsDate/index'
 import { InputsSelectConfig } from './InputsSelect/index'
 import { InputsTabConfig } from './InputsTab/index'
 import {InputsInputConfig} from './InputsInput/index'
+import { InputsPaginationConfig} from "./InputsPagination/index";
 
-export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig,InputsInputConfig]
+export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig,InputsInputConfig,InputsPaginationConfig]
