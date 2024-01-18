3793cdfb05290cf517e7e77e0abb49ecd3c494c6
huanghao1412 <huanghao1412@163.com>
2024-01-18 15:10:13 +0800

    fix: 去掉token

--- src/api/path/business.api.ts
+++ src/api/path/business.api.ts
@@ -6,12 +6,12 @@ import moment from 'moment'
 
 export function getToken() {
     // 为了开发时只单独展示goview 需要localStorage
-    const storage_access_token = localStorage.getItem('access_token_obj')
-    if(storage_access_token) {
-        const obj = JSON.parse(storage_access_token)
-        const { access_token, expiration } = obj
-        if(expiration >= Number(moment().format('x'))) return access_token
-    }
+    // const storage_access_token = localStorage.getItem('access_token_obj')
+    // if(storage_access_token) {
+    //     const obj = JSON.parse(storage_access_token)
+    //     const { access_token, expiration } = obj
+    //     if(expiration >= Number(moment().format('x'))) return access_token
+    // }
     let queryStr = window.location.href
     queryStr = queryStr.split('?')[1]
     if(queryStr && queryStr.indexOf('#') > -1) queryStr = queryStr.split('#')[0]
@@ -19,11 +19,11 @@ export function getToken() {
     queryStr && queryStr.split('&').forEach((item:string) => {
         query[item.split('=')[0]] = item.split('=')[1]
     })
-    const obj = {
-        access_token: query.access_token,
-        expiration: 86400000 + Number(moment().format('x'))
-    }
-    if(query.access_token) localStorage.setItem('access_token_obj', JSON.stringify(obj))
+    // const obj = {
+    //     access_token: query.access_token,
+    //     expiration: 86400000 + Number(moment().format('x'))
+    // }
+    // if(query.access_token) localStorage.setItem('access_token_obj', JSON.stringify(obj))
     return query.access_token
 }
 
