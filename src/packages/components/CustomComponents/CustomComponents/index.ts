9e30c6b6af9cc429e96db5ea0b3087925b5a9957
huanghao1412 <huanghao1412@163.com>
2024-01-24 15:20:34 +0800

    feat: 自定义组件分类

--- src/packages/components/CustomComponents/CustomComponents/index.ts
+++ src/packages/components/CustomComponents/CustomComponents/index.ts
@@ -1,21 +1,15 @@
 // import { Theme1Config } from './Theme1/index'
 import { ComprehensivePUEConfig } from './ComprehensivePUE'
-import { DeviceAlarmOverviewConfig } from './DeviceAlarmOverview'
 import { RealTimeTrafficConfig } from './RealTimeTraffic'
-import { RealTimeAlarmStatisticsConfig } from './RealTimeAlarmStatistics'
 import { OverviewOfComputingNodesConfig } from './OverviewOfComputingNodes'
-import { RealTimeEventConfig } from './RealTimeEvent'
 import { GDMapConfig } from './GDMap'
 import { MonitorRealTimeEventsConfig } from './MonitorRealTimeEvents'
 
 export default [
     // Theme1Config,
     ComprehensivePUEConfig,
-    // DeviceAlarmOverviewConfig,
     RealTimeTrafficConfig,
-    // RealTimeAlarmStatisticsConfig,
     OverviewOfComputingNodesConfig,
-    // RealTimeEventConfig,
     GDMapConfig,
     MonitorRealTimeEventsConfig,
 ]
