02366f3a6c6496701b9a756ce3dfbb00faff427f
huanghao1412 <huanghao1412@163.com>
2024-04-29 11:23:41 +0800

    fix: 视频组件取消默认弹窗

--- src/packages/components/CustomComponents/CustomComponents/VideoList/index.vue
+++ src/packages/components/CustomComponents/CustomComponents/VideoList/index.vue
@@ -84,9 +84,9 @@ const getData = () => {
   publicInterface('/dcim/runhe_ai/event', 'get_current_page', {}).then(res => {
     if (res && res.data) {
       list.value = res.data
-      if(!showDialog && isPreview()) {
-        openVideo(res.data[0] || {})
-      }
+      // if(!showDialog && isPreview()) {
+      //   openVideo(res.data[0] || {})
+      // }
     }
   })
 }
