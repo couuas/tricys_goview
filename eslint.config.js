ed54299736e3943085e266196a2d2a2b35f45761
BaboonKing <changfan.xu.up@gmail.com>
2024-11-20 19:39:35 +0800

    chore: 使 @typescript-eslint/consistent-type-imports 工作

--- eslint.config.js
+++ eslint.config.js
@@ -30,6 +30,11 @@ export default [
   ...vueTsEslintConfig(),
   skipFormatting,
   {
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    },
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
